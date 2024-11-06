2d81fcbe43da297d3ae84606dcded9e627effb1c
hamboomger <hamboomger.dev@gmail.com>
2024-11-06 19:02:24 +0000

    Make files executable

--- log_viewer/copy_over_files.sh
+++ log_viewer/copy_over_files.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-# copy over jenkins local file
+echo 'Copying over logs from jenkins server...'
 mkdir jenkins_local_logs
 
-scp -r -i full_stack_keypair.pem ubuntu@jenkins.n-ws.org:/home/ubuntu/test-dir/logs ./jenkins_local_logs
+scp -r -i /Volumes/Encrypted/full_stack_keypair.pem ubuntu@jenkins.n-ws.org:/home/ubuntu/test-dir/logs ./jenkins_local_logs
 
-# copy over file from remote server
+echo 'Copying over logs from remote web server...'
 mkdir remote_logs
 
 scp -r srv-cslnp8bqf0us7390f4mg@ssh.frankfurt.render.com:/var/data/logs ./remote_logs/
--- log_viewer/show_logs_plot.py
+++ log_viewer/show_logs_plot.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 import pandas as pd
 import plotly.express as px
 from pathlib import Path
 
-file_name = '2024-11-04.csv'
-df = pd.read_csv(Path('./logs') / file_name)
+file_name = '2024-11-05.csv'
+df = pd.read_csv(Path('./jenkins_local_logs') / 'logs' / file_name)
 df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%d-%m-%Y %H:%M:%S')
 
 fig = px.line(df, x = 'Timestamp', y = df.columns[1:])
