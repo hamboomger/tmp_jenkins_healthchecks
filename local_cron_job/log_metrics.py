420a275e576c0fff26e515608e0504c9c911b787
hamboomger <hamboomger.dev@gmail.com>
2024-11-11 11:21:56 +0000

    Rework to use json instead

--- local_cron_job/log_metrics.py
+++ local_cron_job/log_metrics.py
@@ -8,31 +8,38 @@ import requests
 from pathlib import Path
 from datetime import date, datetime
 
-
 logs_dir = Path(__file__).parent.resolve() / 'logs'
 today = str(date.today())
 
 logs_dir.mkdir(exist_ok=True, parents=True)
 
-logs_file = f'{today}.csv'
+logs_file = f'{today}.json'
 file_path = logs_dir / logs_file
 if not file_path.exists():
-    file_path.write_text('Timestamp,Cpu usage,Mem usage,Disk usage\n')
+    file_path.write_text('[]')
 
-with file_path.open('a') as f:
+json_contents: list = []
+with file_path.open('r') as f:
+    json_contents = json.load(f)
+
+def get_json_record():
     naive_datetime = datetime.now()
     server_datetime = naive_datetime.astimezone()
+    return {
+        'timestamp': server_datetime.isoformat(),
+        'mem_percent': psutil.virtual_memory().percent,
+        'cpu_percent': psutil.cpu_percent(interval=5),
+        'disk_usage': psutil.disk_usage(os.sep).percent
+    }
 
-    timestamp = str(server_datetime.isoformat())
-    mem_percent = psutil.virtual_memory().percent
-    cpu_percent = psutil.cpu_percent(interval=5)
-    disk_usage = psutil.disk_usage(os.sep).percent
+json_record = get_json_record()
+json_contents.append(json_record)
 
-    log_entry = f'{timestamp},{cpu_percent},{mem_percent},{disk_usage}\n'
-    f.write(log_entry)
+with file_path.open('w') as f:
+    json.dump(json_contents, f, indent=4)
 
-    res = requests.post('https://tmp-jenkins-healthchecks.onrender.com/log-entry',
-                  json={ 'fileName': logs_file, 'entry': log_entry })
-    res.raise_for_status()
+# res = requests.post('https://tmp-jenkins-healthchecks.onrender.com/log-entry',
+#               json={ 'fileName': logs_file, 'entry': json_record })
+# res.raise_for_status()
 
-    print(f'Added telemetry data for {timestamp}')
+print(f'Added telemetry data for {json_record["timestamp"]}')
