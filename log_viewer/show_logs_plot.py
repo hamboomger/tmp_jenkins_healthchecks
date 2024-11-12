1eefa44320a79f1be9db56aa9f55919cbb73eefa
hamboomger <hamboomger.dev@gmail.com>
2024-11-12 14:25:18 +0000

    Show docker containers running

--- log_viewer/show_logs_plot.py
+++ log_viewer/show_logs_plot.py
@@ -4,20 +4,29 @@ import pandas as pd
 import plotly.express as px
 from pathlib import Path
 
-file_name = '2024-11-08.json'
+file_name = '2024-11-11-custom.json'
 
 local_jenkins_logs = Path('./jenkins_local_logs') / 'logs'
 remote_server_logs = Path('./remote_logs') / 'logs'
 
 
 # Local logs
-df1 = pd.read_csv(local_jenkins_logs / file_name)
+df1 = pd.read_json(local_jenkins_logs / file_name)
+df1['timestamp'] = pd.to_datetime(df1['timestamp']).dt.tz_convert('UTC')
 
-fig1 = px.line(df1, x ='timestamp', y =df1.columns[1:-1], title='Local logs')
+# Flatten the docker info into a string format for hover text
+df1['docker_info'] = df1['docker'].apply(lambda containers: "<br>".join(
+    f"{c['name']}<br>    Mem: {c['mem']}<br>    CPU: {c['cpu']:.2f}%<br>    Command: {' '.join(c['command'])}"
+    for c in containers
+))
+
+fig1 = px.line(df1, x ='timestamp', y=['mem_percent'], title='Local logs', markers=True, hover_data={'docker_info': True})
+
+fig1.update_traces(marker=dict(size=8), hovertemplate='<br>'.join([
+  "Time: %{x}",
+  "Memory Percent: %{y}%",
+  "Docker containers:",
+  "%{customdata[0]}"
+]))
+
 fig1.show()
-
-# Remote logs
-df2 = pd.read_json(remote_server_logs / file_name)
-
-fig2 = px.line(df2, x ='timestamp', y =df2.columns[1:-1], title='Remote logs')
-fig2.show()
